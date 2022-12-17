f7bfcc8961c3176476024b556cf2750f587f6268
Darren Wayn <dwayn.dev@gmail.com>
2022-12-17 12:26:23 +0700

    added simple ui after getting the decks api

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,19 +2,31 @@ import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 
 import Deck from "./models/Deck";
+import cors from "cors";
 import { config } from "dotenv";
 
 config();
-const PORT = 5001;
 
+const PORT = 5001;
 const app = express();
+
+app.use(cors({ origin: "*" }));
+// Express middleware function
+// This helps to understand and parse application/json in headers
+app.use(express.json());
+
+app.get("/decks", async (req: Request, res: Response) => {
+  // Tpdp: fetch all decks and send back to the use
+  // 1. how do we fetch the decks from mongo?
+  const decks = await Deck.find();
+  console.log(decks);
+  // 2. how do we send back the array to the ui?
+  res.json(decks);
+});
 
 app.get("/hello", (req: Request, res: Response) => {
   res.send("hello world");
 });
-
-//Express middleware function
-app.use(express.json());
 
 app.post("/decks", async (req: Request, res: Response) => {
   const newDeck = new Deck({
